aa4319ae07dbf060e60e4002de14688316e5828b
Angelika Jarosz <angelikajarosz@gmail.com>
2015-12-14 16:44:15 -0500

    add django-betterforms to settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -39,6 +39,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'django-betterforms', # Form usability extended
 )
 
 # Apps specific for this project go here.
