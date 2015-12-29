7962f45c23d0f1b912f9761472b629bb7e731178
Angelika Jarosz <angelikajarosz@gmail.com>
2015-12-29 18:40:56 -0500

    register fertility model in admin

--- femstats/fertility/admin.py
+++ femstats/fertility/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from femstats.fertility.models import Period
+from femstats.fertility.models import Period, Fertility
 
 class PeriodAdmin(admin.ModelAdmin):
     list_display = ('user','date','flow')
 
 admin.site.register(Period, PeriodAdmin)
+
+class FertilityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'time', 'temperature',
+                    'position', 'mucus', 'texture', 'opening')
+
+admin.site.register(Fertility, FertilityAdmin)
